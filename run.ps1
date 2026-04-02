2923cf59acfb459765a7fb7d3da8a9617f2b7cd9
Ryan Haas <haasrr@outlook.com>
2026-04-01 21:12:01 -0400

    Update for new name

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 # Activate the virtual environment
-& C:\Users\haasrr\Documents\Code\whisper_transcriber\.venv\Scripts\Activate.ps1
+& C:\Users\haasrr\Documents\Code\whisper_transcriber_gui\.venv\Scripts\Activate.ps1
 
 # Invoke Python
-python C:\Users\haasrr\Documents\Code\whisper_transcriber\whisperx-gui.py
+python C:\Users\haasrr\Documents\Code\whisper_transcriber_gui\whisperx-gui.py
